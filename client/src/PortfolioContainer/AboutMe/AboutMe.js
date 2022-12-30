14d7d662abe2fddd4dc02fdf5f0732ec8bbdd79a
Anjali Gupta <anjaligupta0621@gmail.com>
2022-12-30 13:53:10 -0800

    header added and about me init

---
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react';
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
+import ScrollService from '../../utilities/ScrollService';
+import Animations from '../../utilities/Animations';
+
+function AboutMe(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AboutMe;
