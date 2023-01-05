3a6f1a291a60420b0df54e1c88606ecce37c0fae
Anjali Gupta <anjaligupta0621@gmail.com>
2023-01-04 19:54:43 -0800

    Testimonial component added

---
+++ client/src/PortfolioContainer/Testimonial/Testimonial.js
@@ -0,0 +1,221 @@
+import React from "react";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./Testimonial.css";
+
+function Testimonial(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) {
+      return;
+    }
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const options = {
+    loop: true,
+    margin: 0,
+    nav: true,
+    animateIn: "bounceInRight",
+    animateOut: "bounceOutRight",
+    dots: true,
+    autoplay: true,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <ScreenHeading
+        title={"Testimonial"}
+        subHeading={"What my clients say about me"}
+      />
+      <section className="testimonial-section" id={props.id || ""}>
+        <div className="container">
+          <div className="row">
+            <OwlCarousel
+              className="owl-carousel"
+              id="testimonial-carousel"
+              {...options}
+            >
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I patronized Anjali and when she delivered, I honestly
+                      fell in love with the project. She is very honest and
+                      delivers on time.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img
+                      src="img/testimonial/lady.png"
+                      alt="no internet connection"
+                    />
+                    <h5>Daisy Dominic</h5>
+                    <p>CEO UF Innovate</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I patronized Anjali and when she delivered, I honestly
+                      fell in love with the project. She is very honest and
+                      delivers on time.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img
+                      src="img/testimonial/man.png"
+                      alt="no internet connection"
+                    />
+                    <h5>Man Manny</h5>
+                    <p>CEO Man123</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I patronized Anjali and when she delivered, I honestly
+                      fell in love with the project. She is very honest and
+                      delivers on time.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img
+                      src="img/testimonial/mike.png"
+                      alt="no internet connection"
+                    />
+                    <h5>Mike Jay</h5>
+                    <p>CEO MikeTech</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I patronized Anjali and when she delivered, I honestly
+                      fell in love with the project. She is very honest and
+                      delivers on time.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img
+                      src="img/testimonial/lady.png"
+                      alt="no internet connection"
+                    />
+                    <h5>Daisy Dominic</h5>
+                    <p>CEO UF Innovate</p>
+                  </div>
+                </div>
+              </div>
+            </OwlCarousel>
+          </div>
+        </div>
+      </section>
+      <div className="footer-image">
+        <img src="img/Testimonial/shape-bg.png" />
+      </div>
+    </div>
+  );
+}
+
+export default Testimonial;
--- client/src/utilities/commonUtils.js
+++ client/src/utilities/commonUtils.js
@@ -1,6 +1,7 @@
 import Home from '../PortfolioContainer/Home/Home';
 import AboutMe from '../PortfolioContainer/AboutMe/AboutMe';
 import Resume from '../PortfolioContainer/Resume/Resume';
+import Testimonial from '../PortfolioContainer/Testimonial/Testimonial';
 
 export const TOTAL_SCREENS = [
     {
@@ -14,6 +15,10 @@ export const TOTAL_SCREENS = [
     {
         screen_name: "Resume",
         component: Resume,
+    },
+    {
+        screen_name: "Testimonial",
+        component: Testimonial,
     },
 ];
 
